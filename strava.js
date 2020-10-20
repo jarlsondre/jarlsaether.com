c1c2210483a52aff71d198ada46147799dcf4218
Jarl Sondre Sæther <jssaethe@stud.ntnu.no>
2020-10-20 12:54:51 +0200

    Testing out some stuff

--- strava.js
+++ strava.js
@@ -5,5 +5,4 @@ console.log("Hello World!")
 
 let response = fetch("https://strava.com/api/v3/athletes/55489776/stats?access_token=a46604e4240841d3d5f1c590eea0ce9b7cec3def")
 
-console.log(response)
 
