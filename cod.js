00ee1e83a5865569a4e859732b45fa4310322523
Jarl Sondre Sæther <jssaethe@stud.ntnu.no>
2020-10-21 17:45:04 +0200

    added leaderboard

--- cod.js
+++ cod.js
@@ -1,16 +1,31 @@
 
 
-function addToTable(data) {
+var listOfPlayers = []; 
+
+function updateTable() {
     var tableRef = document.getElementById('playerTable').getElementsByTagName('tbody')[0];
-        var newRow = tableRef.insertRow();
+    var x;
+    for(x = tableRef.rows.length - 1 ; x > 0; x--) {
+        tableRef.deleteRow(x); 
+    }
         var i; 
-        for(i = 0; i < 4; i++) {
-            var newCell = newRow.insertCell(i);
-            var newText = document.createTextNode(data[i]);
-            newCell.appendChild(newText); 
+        var j; 
+        for(j = 0; j < listOfPlayers.length; j++) {
+            var data = listOfPlayers[j]
+            var newRow = tableRef.insertRow();
+            for(i = 0; i < 4; i++) {
+                var newCell = newRow.insertCell(i);
+                var newText = document.createTextNode(data[i]);
+                newCell.appendChild(newText); 
+            }
         }
-        
 }
+
+function sortPlayers() {
+    listOfPlayers.sort(function(a, b) {return b[1] - a[1]});
+
+    }
+
 
 // Function for fetching the json data from Rapid API and displaying it
 function fetchData() {
@@ -26,7 +41,10 @@ function fetchData() {
         return response.json();    
     })
     .then( (data) => {
-        addToTable(formatFetchData(username, data)); 
+        var formatted = formatFetchData(username, data);
+        listOfPlayers.push(formatted); 
+        sortPlayers();
+        updateTable(); 
     })
     .catch(err => {
         console.error(err);
